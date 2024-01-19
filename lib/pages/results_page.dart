427dcc0e2a1e3b24aba7cfcdff6899d6d4983cc2
Joel <86185696+AnJasid@users.noreply.github.com>
2024-01-19 23:57:14 +0800

    update result page: add best score and time

--- lib/pages/results_page.dart
+++ lib/pages/results_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:football_trivia/data/questions.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class ResultsPage extends StatelessWidget {
+class ResultsPage extends StatefulWidget {
   const ResultsPage({
     super.key,
     required this.chosenAnswers,
@@ -13,20 +14,36 @@ class ResultsPage extends StatelessWidget {
   final List<String> chosenAnswers;
   final int elapsedTime;
 
+  @override
+  State<ResultsPage> createState() => _ResultsPageState();
+}
+
+class _ResultsPageState extends State<ResultsPage> {
+  int bestScore = 0;
+  int bestTimeInSeconds = 0;
+
   List<Map<String, Object>> get summaryData {
     final List<Map<String, Object>> summary = [];
 
-    for (var i = 0; i < chosenAnswers.length; i++) {
+    for (var i = 0; i < widget.chosenAnswers.length; i++) {
       summary.add(
         {
           'question_index': i,
           'question': questions[i].questionText,
           'correct_answer': questions[i].answers[0],
-          'user_answer': chosenAnswers[i]
+          'user_answer': widget.chosenAnswers[i]
         },
       );
     }
     return summary;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Load previous best score and best time from SharedPreferences
+    loadBestScore();
+    loadBestTime();
   }
 
   @override
@@ -35,6 +52,18 @@ class ResultsPage extends StatelessWidget {
     final numCorrectQuestions = summaryData
         .where((data) => data['user_answer'] == data['correct_answer'])
         .length;
+
+    // Check if the current score is higher than the best score
+    if (numCorrectQuestions > bestScore) {
+      // Update best score in SharedPreferences
+      saveBestScore(numCorrectQuestions);
+    }
+
+    // Check if the current time is shorter than the best time
+    if (widget.elapsedTime < bestTimeInSeconds || bestTimeInSeconds == 0) {
+      // Update best time in SharedPreferences
+      saveBestTime(widget.elapsedTime);
+    }
 
     return SizedBox(
       width: double.infinity,
@@ -88,7 +117,17 @@ class ResultsPage extends StatelessWidget {
                         children: [
                           Text('$numCorrectQuestions'),
                           Text(
-                              '${(elapsedTime ~/ 3600).toString().padLeft(2, '0')}:${((elapsedTime % 3600) ~/ 60).toString().padLeft(2, '0')}:${(elapsedTime % 60).toString().padLeft(2, '0')}'),
+                              '${(widget.elapsedTime ~/ 3600).toString().padLeft(2, '0')}:${((widget.elapsedTime % 3600) ~/ 60).toString().padLeft(2, '0')}:${(widget.elapsedTime % 60).toString().padLeft(2, '0')}'),
+                          const SizedBox(height: 10),
+                          // Show best score and best time widgets
+                          Text(
+                            'Best Score: $bestScore',
+                            style: const TextStyle(fontSize: 16),
+                          ),
+                          Text(
+                            'Best Time: ${bestTimeInSeconds ~/ 60}:${bestTimeInSeconds % 60}',
+                            style: const TextStyle(fontSize: 16),
+                          ),
                         ],
                       ),
                     ),
@@ -100,7 +139,7 @@ class ResultsPage extends StatelessWidget {
             // QuestionsSummary(summaryData),
             const SizedBox(height: 30),
             TextButton.icon(
-              onPressed: onRestart,
+              onPressed: widget.onRestart,
               style: TextButton.styleFrom(
                 foregroundColor: Colors.white,
               ),
@@ -116,5 +155,29 @@ class ResultsPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  // Function to save the best score to SharedPreferences
+  Future<void> saveBestScore(int score) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setInt('bestScore', score);
+  }
+
+  // Function to load the best score from SharedPreferences
+  Future<void> loadBestScore() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bestScore = prefs.getInt('bestScore') ?? 0;
+  }
+
+  // Function to save the best time to SharedPreferences
+  Future<void> saveBestTime(int time) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setInt('bestTimeInSeconds', time);
+  }
+
+  // Function to load the best time from SharedPreferences
+  Future<void> loadBestTime() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bestTimeInSeconds = prefs.getInt('bestTimeInSeconds') ?? 0;
   }
 }
