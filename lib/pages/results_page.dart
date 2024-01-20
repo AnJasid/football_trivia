dff4edf8590ab9cba3abeb32c4b746dbdc99baca
Joel <86185696+AnJasid@users.noreply.github.com>
2024-01-20 21:34:53 +0800

    update UI for result screen

--- lib/pages/results_page.dart
+++ lib/pages/results_page.dart
@@ -76,69 +76,173 @@ class _ResultsPageState extends State<ResultsPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            SizedBox(
-              height: 521,
+            Container(
               width: double.infinity,
-              child: Stack(
+              height: 400,
+              decoration: BoxDecoration(
+                color: const Color(0xff38649c),
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(
+                  color: Colors.white, // Specify the color of the border
+                  width: 3,
+                ),
+              ),
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container(
-                    height: 340,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.amber,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Center(
-                      child: CircleAvatar(
-                        radius: 85,
-                        backgroundColor: Colors.white.withOpacity(0.3),
-                        child: CircleAvatar(
-                          radius: 71,
-                          backgroundColor: Colors.white.withOpacity(0.4),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              const Text('Your Score'),
-                              Text('${numCorrectQuestions * 10}%'),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 70,
-                    left: 20,
-                    child: Container(
-                      height: 190,
-                      width: 330,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Column(
-                        children: [
-                          Text('Score: $numCorrectQuestions'),
-                          Text(
-                              'Best Score: ${(widget.elapsedTime ~/ 3600).toString().padLeft(2, '0')}:${((widget.elapsedTime % 3600) ~/ 60).toString().padLeft(2, '0')}:${(widget.elapsedTime % 60).toString().padLeft(2, '0')}'),
-                          const SizedBox(height: 10),
-                          // Show best score and best time widgets
-                          Text(
-                            'Best Score: $bestScore',
-                            style: const TextStyle(fontSize: 16),
-                          ),
-                          Text(
-                            'Best Time: ${bestTimeInSeconds ~/ 60}:${bestTimeInSeconds % 60}',
-                            style: const TextStyle(fontSize: 16),
-                          ),
-                        ],
-                      ),
-                    ),
+                  Divider(
+                    color: Colors.white,
+                    thickness: 2,
+                    height: 40,
                   ),
                 ],
               ),
             ),
+
+            // SizedBox(
+            //   height: 521,
+            //   width: double.infinity,
+            //   child: Stack(
+            //     children: [
+            //       Container(
+            //         height: 340,
+            //         width: double.infinity,
+            //         decoration: BoxDecoration(
+            //           color: Colors.amber,
+            //           borderRadius: BorderRadius.circular(20),
+            //         ),
+            //         child: Center(
+            //           child: CircleAvatar(
+            //             radius: 85,
+            //             backgroundColor: Colors.white.withOpacity(0.3),
+            //             child: CircleAvatar(
+            //               radius: 71,
+            //               backgroundColor: Colors.white.withOpacity(0.4),
+            //               child: Column(
+            //                 mainAxisAlignment: MainAxisAlignment.center,
+            //                 crossAxisAlignment: CrossAxisAlignment.center,
+            //                 children: [
+            //                   const Text('Your Score'),
+            //                   Text('${numCorrectQuestions * 10}%'),
+            //                 ],
+            //               ),
+            //             ),
+            //           ),
+            //         ),
+            //       ),
+            //       Positioned(
+            //         bottom: 70,
+            //         left: 20,
+            //         child: Container(
+            //           height: 190,
+            //           width: 330,
+            //           decoration: BoxDecoration(
+            //             color: Colors.white,
+            //             borderRadius: BorderRadius.circular(20),
+            //           ),
+            //           child: Column(
+            //             children: [
+            //               Text('Score: $numCorrectQuestions'),
+            //               Text(
+            //                   'Best Score: ${(widget.elapsedTime ~/ 3600).toString().padLeft(2, '0')}:${((widget.elapsedTime % 3600) ~/ 60).toString().padLeft(2, '0')}:${(widget.elapsedTime % 60).toString().padLeft(2, '0')}'),
+            //               const SizedBox(height: 10),
+            //               // Show best score and best time widgets
+            //               Text(
+            //                 'Best Score: $bestScore',
+            //                 style: const TextStyle(fontSize: 16),
+            //               ),
+            //               Text(
+            //                 'Best Time: ${bestTimeInSeconds ~/ 60}:${bestTimeInSeconds % 60}',
+            //                 style: const TextStyle(fontSize: 16),
+            //               ),
+            //             ],
+            //           ),
+            //         ),
+            //       ),
+            //     ],
+            //   ),
+            // ),
+            const SizedBox(height: 30),
+            const Center(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 18),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: Color(0xff37AFA1),
+                          radius: 35,
+                          child: Center(
+                            child: Icon(
+                              Icons.refresh,
+                              size: 35,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          'Restart',
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: Color(0xff37AFA1),
+                          radius: 35,
+                          child: Center(
+                            child: Icon(
+                              Icons.visibility_rounded,
+                              size: 35,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          'View Answer',
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: Color(0xff37AFA1),
+                          radius: 35,
+                          child: Center(
+                            child: Icon(
+                              Icons.home,
+                              size: 35,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          'Home',
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
             const SizedBox(height: 30),
             // QuestionsSummary(summaryData),
             const SizedBox(height: 30),
