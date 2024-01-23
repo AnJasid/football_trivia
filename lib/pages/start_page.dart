fe9e83f1b6de015d6993e903820d178e2c3c2779
Joel <86185696+AnJasid@users.noreply.github.com>
2024-01-23 20:17:20 +0800

    update on start page: Begin button

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -18,23 +18,57 @@ class StartPage extends StatelessWidget {
             color: const Color.fromARGB(150, 255, 255, 255),
           ),
           const SizedBox(height: 80),
-          Text(
-            'Text Here',
-            style: GoogleFonts.lato(
-              color: const Color.fromARGB(255, 237, 223, 252),
-              fontSize: 24,
-            ),
-          ),
+          // Text(
+          //   'Text Here',
+          //   style: GoogleFonts.lato(
+          //     color: const Color.fromARGB(255, 237, 223, 252),
+          //     fontSize: 24,
+          //   ),
+          // ),
           const SizedBox(height: 30),
-          ElevatedButton(
-            onPressed: startQuiz,
-            child: const Text(
-              'Begin',
-              style: TextStyle(
-                color: Colors.black,
+          GestureDetector(
+            onTap: startQuiz,
+            child: Container(
+              // ignore: prefer_const_constructors
+              padding: EdgeInsets.symmetric(
+                horizontal: 50,
+                vertical: 8,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(
+                  color: Colors.white,
+                  width: 3,
+                ),
+                gradient: const LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color.fromARGB(255, 126, 200, 218),
+                    Color(0xff0066ff),
+                  ],
+                ),
+              ),
+              child: const Text(
+                'BEGIN',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
             ),
           ),
+          // ElevatedButton(
+          //   style: ElevatedButton.styleFrom().copyWith(),
+          //   onPressed: startQuiz,
+          //   child: const Text(
+          //     'BEGIN',
+          //     style: TextStyle(
+          //       color: Colors.black,
+          //     ),
+          //   ),
+          // ),
         ],
       ),
     );
