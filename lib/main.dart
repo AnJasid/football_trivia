a49451e326207d82427779cfe4632784b39421f7
Joel <86185696+AnJasid@users.noreply.github.com>
2024-01-24 19:55:56 +0800

    add OneSignal SDK

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:football_trivia/pages/quiz.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  OneSignal.initialize("27350c67-5d07-4899-b450-10b7425fd642");
+  OneSignal.Notifications.requestPermission(true);
+
   // to set the orientation to portrait
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
